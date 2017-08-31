3bdaf132d486c3d0771f228b924cb5dafb16b0d1
vasmethk <vasmethk@indiana.edu>
2017-08-31 02:04:22 -0400

    made changes to python flas microservice

--- Assignment-1/python-service/app.py
+++ Assignment-1/python-service/app.py
@@ -1,11 +1,24 @@
 #!flask/bin/python
 from flask import Flask
-
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World! You are in Python Flask Microservice"
+app.config['MONGO_DBNAME']='bookstore'
+app.config['MONGO_URI']='mongodb://localhost:27017/bookstore'
+
+mongo = PyMongo(app)
+
+@app.route('/aj')
+def get_all_books():
+	book=mongo.db.books
+	output = []
+	for a in book.find():
+		output.append({'author' : a['author'], 'create_date' : a['create_date'], 'description':a['description'], 'genre': a['genres']})
+	return jsonify({'result':output})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
