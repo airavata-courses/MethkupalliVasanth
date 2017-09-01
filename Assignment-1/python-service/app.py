c0ab028a055c275e1ebc3e2d44e6ab2a58bfce4d
vasmethk <vasmethk@indiana.edu>
2017-09-01 14:27:18 -0400

    Adding orchestrator

--- Assignment-1/python-service/app.py
+++ Assignment-1/python-service/app.py
@@ -3,21 +3,27 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask_pymongo import PyMongo
+
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME']='bookstore'
 app.config['MONGO_URI']='mongodb://localhost:27017/bookstore'
 
+
 mongo = PyMongo(app)
 
-@app.route('/aj')
+@app.route('/')
 def get_all_books():
 	book=mongo.db.books
-	output = []
-	for a in book.find():
-		output.append({'author' : a['author'], 'create_date' : a['create_date'], 'description':a['description'], 'genre': a['genres']})
-	return jsonify({'result':output})
 
+	output = []
+	#return collection
+	#return mongo.db.bookstore
+	for a in book:
+	 	output.append({'author' : a['author'], 'create_date' : a['create_date'], 'description':a['description'], 'genre': a['genres']})
+		#ouput.append({'author':a['author']})
+	return jsonify({'result':output})
+	#return "hello"
 
 
 if __name__ == '__main__':
