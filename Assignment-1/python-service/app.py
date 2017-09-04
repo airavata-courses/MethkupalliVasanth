c8f4c65ca9277759e673066767fbf2961385815c
vasmethk <vasmethk@indiana.edu>
2017-09-04 17:44:43 -0400

    Architecture Diagram

--- Assignment-1/python-service/app.py
+++ Assignment-1/python-service/app.py
@@ -26,5 +26,11 @@ def get_all_books():
 	#return "hello"
 
 
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
