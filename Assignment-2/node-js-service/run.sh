4a8add85fb4e70016d62bd71c428170694f3e8a4
vasmethk <mvasanthiiit@gmail.com>
2017-09-18 23:02:38 -0400

    Update run.sh

--- Assignment-2/node-js-service/run.sh
+++ Assignment-2/node-js-service/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo docker rm -f app_node
+sudo docker rmi msn
+
 sudo docker build -t msn .
 
-sudo docker run -it -p 3000 --net="host" --name app_node  msn
+sudo docker run -d -p 3000 --net="host" --name app_node  msn
