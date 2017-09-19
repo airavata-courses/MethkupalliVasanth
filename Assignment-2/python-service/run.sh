127fcb5850153881f96b1395ba020ea6a7682505
vasmethk <mvasanthiiit@gmail.com>
2017-09-18 23:01:44 -0400

    Update run.sh

--- Assignment-2/python-service/run.sh
+++ Assignment-2/python-service/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo docker rm -f app_python
+sudo docker rmi msp
+
 sudo docker build -t msp .
 
-sudo docker run -it -p 5000 --net="host" --name app_python  msp
+sudo docker run -d -p 5000 --net="host" --name app_python  msp
